7236de4f83c299ec30a00fd1b03a7ba76710be83
Asadullah Khan <asadmuhammad427@gmail.com>
2021-11-04 15:14:26 +0500

    initial commit

---
+++ src/config.js
@@ -0,0 +1,116 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "constants/blend_mode.js"));
+const { NETWORK } = require(path.join(basePath, "constants/network.js"));
+
+const network = NETWORK.eth;
+
+// General metadata for Ethereum
+const namePrefix = "Mirinda Bored Ape";
+const description = "Mirinda Bored Ape NFT collection comming soon...";
+// const baseUri = "ipfs://NewUriToReplace";
+const baseUri = "https://metadata.com";
+
+
+
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 10,
+    layersOrder: [
+      { name: "Backgrounds" },
+      { name: "Head" },
+      { name: "Body" },
+      { name: "Mouth" },
+      { name: "Eyes" },
+      { name: "Hat" },
+      { name: "Earring" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 512,
+  height: 512,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {
+
+};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  gif,
+  preview_gif,
+};
---
+++ utils/update_info.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { NETWORK } = require(path.join(basePath, "constants/network.js"));
+const fs = require("fs");
+
+console.log(path.join(basePath, "/src/config.js"));
+const {
+  baseUri,
+  description,
+  namePrefix,
+  network,
+} = require(path.join(basePath, "/src/config.js"));
+
+// read json data
+let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
+let data = JSON.parse(rawdata);
+
+data.forEach((item) => {
+  if (network == NETWORK.eth) {
+    item.name = `${namePrefix} #${item.edition}`;
+    item.description = description;
+    item.image = `${baseUri}/${item.edition}.png`;
+  }
+  fs.writeFileSync(
+    `${basePath}/build/json/${item.edition}.json`,
+    JSON.stringify(item, null, 2)
+  );
+});
+
+fs.writeFileSync(
+  `${basePath}/build/json/_metadata.json`,
+  JSON.stringify(data, null, 2)
+);
+
+if (network == NETWORK.eth) {
+  console.log(`Updated baseUri for images to ===> ${baseUri}`);
+  console.log(`Updated description for images to ===> ${description}`);
+  console.log(`Updated name prefix for images to ===> ${namePrefix}`);
+}
